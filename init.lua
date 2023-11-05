5ac3be16391c85b1453de3f087758b0269b3345c
Thomas Mader <thomas.mader@gmail.com>
2023-11-05 14:52:11 +0100

    Restructure config and add some general options

--- init.lua
+++ init.lua
@@ -1,8 +1,19 @@
-if vim.g.neovide then
-  vim.g.neovide_cursor_trail_legnth = 0
-  vim.g.neovide_cursor_animation_length = 0
-end
+vim.cmd.language('en_US')
 
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-require("config.lazy")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
---
+++ plugin/options.lua
@@ -0,0 +1,35 @@
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+opt.mouse = 'a'
+opt.showmatch = true
+opt.incsearch = true
+opt.hlsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.scrolloff = 10
+opt.history = 1000
+
+--opt.colorcolumn = '80'
+--opt.smoothscroll = true
+
+opt.laststatus = 2
+
+opt.autoindent = true
+opt.breakindent = true
+
+opt.clipboard = 'unnamedplus'
+
+opt.undofile = true
+
+opt.signcolumn = 'auto'
+
+opt.completeopt = 'menuone,noselect'
+
+opt.termguicolors = true
+
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.tabstop = 4
