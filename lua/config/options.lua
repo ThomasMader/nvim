4c5c09348d474cffd21dda7982b9a98239e986a0
Thomas Mader <thomas.mader@gmail.com>
2023-12-31 15:00:49 +0100

    Restructure config and add plugins from kickstart.nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,15 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_trail_legnth = 0
+  vim.g.neovide_cursor_animation_length = 0
+end
+
 local opt = vim.opt
+
+-- TODO Look into the following options:
+-- https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/plugin/options.lua
 
 opt.relativenumber = true
 opt.number = true
@@ -11,7 +22,8 @@ opt.smartcase = true
 opt.scrolloff = 10
 opt.history = 1000
 
---opt.colorcolumn = '80'
+opt.colorcolumn = '80'
+-- Doesn't currently work for whatever reason. Maybe related to some kickstart.nvim stuff I have added.
 --opt.smoothscroll = true
 
 opt.laststatus = 2
@@ -23,7 +35,7 @@ opt.clipboard = 'unnamedplus'
 
 opt.undofile = true
 
-opt.signcolumn = 'auto'
+opt.signcolumn = 'yes'
 
 opt.completeopt = 'menuone,noselect'
 
@@ -33,3 +45,5 @@ opt.expandtab = true
 opt.shiftwidth = 4
 opt.softtabstop = 4
 opt.tabstop = 4
+
+opt.updatetime = 1000
--- plugin/neovide.lua
+++
@@ -1,5 +0,0 @@
-if vim.g.neovide then
-  vim.g.neovide_cursor_trail_legnth = 0
-  vim.g.neovide_cursor_animation_length = 0
-end
-
